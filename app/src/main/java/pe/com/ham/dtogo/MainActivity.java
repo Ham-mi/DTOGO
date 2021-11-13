b56648d7e5058078ed242ad7ee4c57df564e9709
Ham-mi <66135806+Ham-mi@users.noreply.github.com>
2021-11-14 03:25:04 +0900

    fragment 추가

--- app/src/main/java/pe/com/ham/dtogo/MainActivity.java
+++ app/src/main/java/pe/com/ham/dtogo/MainActivity.java
@@ -1,24 +1,84 @@
 package pe.com.ham.dtogo;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+
+    TabLayout tabs;
+    TabItem tab1;
+    TabItem tab2;
+    TabItem tab3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 액션바 설정
+//        toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        //액션바 기본 타이틀 보여지지 않게
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayShowTitleEnabled(false);
         getSupportActionBar().hide();
 
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        tabs = findViewById(R.id.tabs);
+        tab1 = findViewById(R.id.tab1);
+        tab2 = findViewById(R.id.tab2);
+        tab3 = findViewById(R.id.tab3);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.d("D", "onTabSelected: "+position);
+
+                Fragment selected = null;
+                if(position==0) selected = fragment1;
+                else if(position==1) selected = fragment2;
+                else if(position==2) selected = fragment3;
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
 }
