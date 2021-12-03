c236bd373075627665b45f25cfce383bbf168974
jaehooonjung <58425812+jaehooonjung@users.noreply.github.com>
2021-12-03 21:39:40 +0900

    fragment2
    211203_21:39
    fragment2, item, adater2
    lecture

---
+++ app/src/main/java/pe/com/ham/dtogo/Adapter2.java
@@ -0,0 +1,51 @@
+package pe.com.ham.dtogo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Adapter2 extends RecyclerView.Adapter<Adapter2.ViewHolder> {
+    private ArrayList<String> mData = null;
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView textView1;
+
+        ViewHolder(View itemView){
+            super(itemView);
+
+            textView1 = itemView.findViewById(R.id.text1);
+        }
+    }
+
+    Adapter2(ArrayList<String> list) {
+        mData = list;
+    }
+
+    @NonNull
+    @Override
+    public Adapter2.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater)  context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View view = inflater.inflate(R.layout.recyclerview_item2,  parent, false);
+        Adapter2.ViewHolder vh = new Adapter2.ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter2.ViewHolder holder, int position) {
+        String text = mData.get(position);
+        holder.textView1.setText(text);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+}
--- app/src/main/java/pe/com/ham/dtogo/Fragment2.java
+++ app/src/main/java/pe/com/ham/dtogo/Fragment2.java
@@ -4,13 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment2,container,false);
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0;i<100;i++)
+        {
+            list.add(String.format("TEXT %d", i));
+
+        }
+        RecyclerView recyclerView = view.findViewById(R.id.recycler1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        Adapter2 adapter2 = new Adapter2(list);
+        recyclerView.setAdapter(adapter2);
 
         return view;
     }
