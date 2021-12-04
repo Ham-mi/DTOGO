de188b76bb611b5de76b5cec96fbce73a9e64a29
Ham-mi <66135806+Ham-mi@users.noreply.github.com>
2021-12-04 22:02:18 +0900

    error false

--- app/src/main/java/pe/com/ham/dtogo/dao/AppDatabase.java
+++ app/src/main/java/pe/com/ham/dtogo/dao/AppDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Dday.class, Goal.class, Todo.class}, version = 1)
+@Database(entities = {Dday.class, Goal.class, Todo.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract DdayDao ddayDao();
     public abstract GoalDao goalDao();
