d6f461925950cf539a28f9a0e88583d3460d8b7e
Ham-mi <66135806+Ham-mi@users.noreply.github.com>
2021-12-09 20:53:10 +0900

    intent1_show, intent2

---
+++ app/src/main/java/pe/com/ham/dtogo/Intent1_show.java
@@ -0,0 +1,14 @@
+package pe.com.ham.dtogo;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Intent1_show extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
---
+++ app/src/main/java/pe/com/ham/dtogo/Intent2.java
@@ -0,0 +1,13 @@
+package pe.com.ham.dtogo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Intent2 extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intent2);
+    }
+}
